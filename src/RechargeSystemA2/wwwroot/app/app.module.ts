edd9ec07151f3ad2b16a9bcdb8039136388eaa07
grvdixit16 <grvdixit16@gmail.com>
2017-12-18 23:26:36 +0530

    Add project files.

---
+++ src/RechargeSystemA2/wwwroot/app/app.module.ts
@@ -0,0 +1,41 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { routing } from './app.routing';
+import { AppConfig } from './app.config';
+
+import { AlertComponent } from './shared/index';
+import { AuthGuard } from './guards/index';
+import { AlertService, AuthenticationService, UserService } from './services/index';
+import { HomeComponent } from './home/index';
+import { LoginComponent } from './shared/index';
+import { RegisterComponent } from './shared/index';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        routing
+    ],
+    declarations: [
+        AppComponent,
+        AlertComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent
+    ],
+    providers: [
+        AppConfig,
+        AuthGuard,
+        AlertService,
+        AuthenticationService,
+        UserService
+    ],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
---
+++ src/RechargeSystemA2/wwwroot/app/app.routing.ts
@@ -0,0 +1,17 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/index';
+import { LoginComponent } from './shared/index';
+import { RegisterComponent } from './shared/index';
+import { AuthGuard } from './guards/index';
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
